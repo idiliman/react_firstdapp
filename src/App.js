6bba29dff0a11716e84596abc5c7cc6e2f7501d0
idil <idil.iman1999@gmail.com>
2022-11-22 00:50:18 +0800

    function cleanup

--- src/App.js
+++ src/App.js
@@ -1,49 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { ethers } from "ethers";
-import ABI from "./mood_abi"
+import ABI from "./mood_abi";
 
 export default function App() {
-  const MoodContractAddress = "0x494EFcAA2ec2D37659e6694E8dEbbd60ae9a631E";
-  const MoodContractABI = ABI;
-  const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli")
-  let MoodContract;
-  let signer;
+  const [mood, updateMood] = useState("");
 
-  provider.send("eth_requestAccounts", []).then(() => {
-    provider.listAccounts().then((accounts) => {
+  const setMood = async () => {
+    if (mood == "") {
+      alert("Cannot set");
+    } else if (mood !== "") {
+      alert("done!");
+      console.log(mood);
+    }
 
-      signer = provider.getSigner(accounts[0]);
-
-      MoodContract = new ethers.Contract(
-      MoodContractAddress,
-      MoodContractABI,
-      signer
-      );
-    });
-  });
-
-  async function setMood() {
-    const mood = document.getElementById("mood").value;
-    const setMoodPromise = MoodContract.setMood(mood);
-    await setMoodPromise;
-  }
+    // const setMoodPromise = MoodContract.setMood(mood);
+    // await setMoodPromise;
+  };
 
   async function getMood() {
-    const getMoodPromise = MoodContract.getMood();
-    const Mood = await getMoodPromise;
-    console.log(Mood);
+    // const getMoodPromise = MoodContract.getMood();
+    // const Mood = await getMoodPromise;
+    console.log("getMood");
   }
 
   return (
     <container>
-    <body>
-      <div>
-        <h1>This is my dApp!</h1>
-        <p className="">Here we can set or get the mood:</p>
-        <label for="mood">Input Mood:</label> <br />
-        <input className="border-solid border-2 border-slate-600" type="text" id="mood" />
-      </div>
-    </body>
-  </container>
-  )
+      <body>
+        <div>
+          <h1>This is my dApp!</h1>
+          <p className="">Here we can set or get the mood:</p>
+          <label for="mood">Input Mood:</label> <br />
+          <input
+            className="border-solid border-2 border-slate-600"
+            onChange={(event) => updateMood(event.target.value)}
+            type="text"
+            id="mood"
+          />
+          <button onClick={setMood}>Set Mood</button>
+          <button onClick={getMood}>Get Mood</button>
+        </div>
+      </body>
+    </container>
+  );
 }
---
+++ src/function/Provider.js
@@ -0,0 +1,17 @@
+// const MoodContractAddress = "0x494EFcAA2ec2D37659e6694E8dEbbd60ae9a631E";
+  // const MoodContractABI = ABI;
+  // const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+  // let MoodContract;
+  // let signer;
+
+  // provider.send("eth_requestAccounts", []).then(() => {
+  //   provider.listAccounts().then((accounts) => {
+  //     signer = provider.getSigner(accounts[0]);
+
+  //     MoodContract = new ethers.Contract(
+  //       MoodContractAddress,
+  //       MoodContractABI,
+  //       signer
+  //     );
+  //   });
+  // });
